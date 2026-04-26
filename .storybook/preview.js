f37b0fecdc5f592955e49194cd847e429dd3020f
Clawdia <clawdiacool@gmail.com>
2026-04-26 23:23:34 +0700

    improvement(storybook): add theme selector toolbar for dark mode preview
    - Add @storybook/addon-themes dependency
    - Configure withThemeByDataAttribute decorator in preview.js
    - Uses data-theme attribute (light/dark) to toggle themes
    - Allows previewing all 32 components in both light and dark modes
    
    Closes #71

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import '../src/theme.css';
 import '../src/index.ts';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
 
 /** @type { import('@storybook/web-components').Preview } */
 const preview = {
@@ -11,6 +12,16 @@ const preview = {
         date: /Date$/i
       }
     }
-  }
+  },
+  decorators: [
+    withThemeByDataAttribute({
+      themes: {
+        light: 'light',
+        dark: 'dark'
+      },
+      defaultTheme: 'light',
+      attributeName: 'data-theme'
+    })
+  ]
 };
 export default preview;
